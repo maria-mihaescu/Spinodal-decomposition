64cd7413d270c3e1fa5f9188caf29dd9ff9891b3
Maria Mihaescu <mariachani.mihaescu@studio.unibo.it>
2023-05-29 14:40:20 +0200

    corrected problems lists/arrays in composition, eta and temperature

--- Binary_Alloys.py
+++ Binary_Alloys.py
@@ -26,6 +26,29 @@ def xlog(x):
     else:
         return 0
     
+#make the plots in order to do the animation rotating in 3d
+def plot_anim_3d(X,Y,Z,xlabel,ylabel,zlabel,title):
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_zlabel(zlabel)
+    ax.set_title(title)
+
+    wframe = None
+    tstart = time.time()
+    for phi in np.linspace(0, 180. / np.pi, 100):
+        # If a line collection is already remove it before drawing.
+        if wframe:
+            wframe.remove()
+        # Generate data.
+        Z = np.cos(2 * np.pi * X + phi) * (1 - np.hypot(X, Y))
+        # Plot the new wireframe and pause briefly before continuing.
+        wframe = ax.plot_surface(X, Y, Z,cmap=cm.jet, rstride=2, cstride=2)
+        #plt.pause(.001)
+    
+    print('Average FPS: %f' % (100 / (time.time() - tstart)))
+    
 #This program calculates the free energy of a binary alloy in the
 # so-called "quasi-chemical" atomistic model. Only nearest-neighbours
 # interactions are taken into account and only the configurational 
@@ -45,52 +68,68 @@ eta=np.arange(-0.5,0.5,0.01) # Order parameter (structural)
 # Functions in (X_B,eta) space for T=T0;
 
 x_BG,etaG=np.meshgrid(X_B,eta)  # defines the (X_B, eta) grid
-H0=N_A*omega*(x_BG*(1-x_BG)+eta**2) #  Enthalpy
+H0=[]
+S0=[]
+G0=[]
 
 # Uses the xlog function, that handles the cases where the argument of the
 # logarithm would be negative, to calculate entropy
-
-S0=-(R_gas/2)*(xlog(x_BG+etaG)+xlog(x_BG-etaG)+xlog(1-x_BG+etaG)+xlog(1-x_BG-etaG))
-
 #  Finds the unphysical region in the (X_B, eta) mesh. In fact, abs(eta)
 #  must be lower than X_B and 1-X_B (see definitions in the model)
-
-#nosense=logical_or((abs(etaG)>=x_BG),(abs(etaG)>=(1-x_BG)));
-
 # Sets enthalpy and entropy to "Not a Number" in the unphysical region, to
 # avoid plotting of non physical values
 
-# for etag, xbg in zip(etaG,x_BG):
-#     if (abs(etag.all())>=xbg.all()) or (abs(etag.all())>=(1-xbg.all())):
-#         H0=float('nan')
-#         S0=float('nan')
 
-
-G0=H0-T0*S0 # Gibbs free energy
-
+for xbg,etag in zip(x_BG,etaG):
+    h0=[N_A*omega*(x*(1-x)+e**2) for x,e in zip(xbg,etag) if (abs(e)< x) or (abs(e)<(1-x))]
+    H0.append(h0) #  Enthalpy
+    s0=[-(R_gas/2)*(xlog(x+e)+xlog(x-e)+ xlog(1-x+e)+xlog(1-x-e))
+        for x,e in zip(xbg,etag) if (abs(e)< x) or (abs(e)<(1-x))]
+    S0.append(s0)
+    g0=[h-T0*s for h,s in zip(h0,s0)]
+    G0.append(g0)
+    
+print('H0 =',H0)
 
 
 # Functions in (X_B,T) space for eta=0
 
+H1=[]
+TS1=[]
+G1=[]
 xG, yG = np.meshgrid(X_B,T)
 
-H1=N_A*omega*(xG*(1-xG)) # enthalpy
-
-TS1=-R_gas*(yG*(xlog(xG)+xlog(1-xG))) #entropy
-
-G1=H1-TS1;  #Gibbs freee energy
-
+for xg,yg in zip(xG,yG):
+    h1=[N_A*omega*(x*(1-x)) for x in xg]
+    H1.append(h1) #  Enthalpy
+    
+    ts1=[-R_gas*(y*(xlog(x)+xlog(1-x))) for x,y in zip(xg,yg)]
+    TS1.append(ts1) #entropy
+    
+    g1=[h-ts for h,ts in zip(h1,ts1)]
+    G1.append(g1) #Gibbs freee energy
+    
 #Functions in (eta,T) space for X_B=0.5
 
 eG, TG = np.meshgrid(eta,T)
-H2=N_A*omega*(0.25+eG**2) # enthalpy
-TS2=-R_gas*(yG*(xlog(0.5+eG)+xlog(0.5-eG))) # entropy
-G2=H2-TS2 #Gibbs free energy
+H2=[]
+TS2=[]
+G2=[]
 
-
+for eg,tg, yg in zip(eG,TG,yG):
+    h2=[N_A*omega*(0.25+e**2) for e in eg]
+    H2.append(h2) #  Enthalpy
+    
+    ts2=[-R_gas*(y*(xlog(0.5+e)+xlog(0.5-e))) for e,y in zip(eg,yg)]
+    TS2.append(ts2) #entropy
+    
+    g2=[h-ts for h,ts in zip(h2,ts2)]
+    G2.append(g2) #Gibbs freee energy
+    
 # GRAPHICS ===============================================================
 
 # Free energy vs composition at different T for order parameter eta=0
+
 plt.figure()
 for g1 in G1:
     plt.plot(X_B,g1)
@@ -101,6 +140,7 @@ plt.show()
 
 
 # Free energy vs order paramer eta for different T at X_B=0.5
+
 plt.figure()
 for g2 in G2:
     plt.plot(eta,g2)
@@ -112,30 +152,17 @@ plt.show()
 
 
 # Free energy surface in (X_B,eta) space for T=T0 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
+plot_anim_3d(x_BG,etaG,G0,
+             'X_B','eta','G [ J/mole ]'
+             ,'G vs X_B and eta, N_A\Omega={:.2f} J , T={:.2f}'.format(N_A*omega,T0))
 
-# Rename the data
-X, Y, Z = x_BG,etaG,G0
+# Free energy surface in (X_B,T) space for order parameter eta=0 
+plot_anim_3d(xG,yG,G1,
+             'X_B','T [K]','G [ J/mole ]'
+             ,'G vs X_B and T, eta=0, N_A\Omega={:.2f} J'.format(N_A*omega))
 
-# Set the axis labels and title
-ax.set_xlabel('X_B')
-ax.set_ylabel('eta')
-ax.set_zlabel('G [ J/mole ]')
-ax.set_title('G vs X_B and eta, N_A\Omega={:.2f} J , T={:.2f}'.format(N_A*omega,T0))
-
-#make the plots in order to do the animation rotating in 3d
-wframe = None
-tstart = time.time()
-for phi in np.linspace(0, 180. / np.pi, 100):
-    # If a line collection is already remove it before drawing.
-    if wframe:
-        wframe.remove()
-    # Generate data.
-    Z = np.cos(2 * np.pi * X + phi) * (1 - np.hypot(X, Y))
-    # Plot the new wireframe and pause briefly before continuing.
-    wframe = ax.plot_surface(X, Y, Z,cmap=cm.jet, rstride=2, cstride=2)
-    #plt.pause(.001)
-
-print('Average FPS: %f' % (100 / (time.time() - tstart)))
+#Free energy surface in (eta,T) space for equimolar composition (X_B=0.5)
+plot_anim_3d(eG,TG,G2,
+             'eta','T [K]','G [ J/mole ]'
+             ,'G vs eta and T, X_B=0.5, N_A\Omega={:.2f} J'.format(N_A*omega))
 
