c048cc34e845cb5b4820d57ffb120a58ba882ec5
Maria Mihaescu <mariachani.mihaescu@studio.unibo.it>
2023-05-30 13:07:07 +0200

    correcting errors and expliciting boundary conditions for the laplacian

--- Cahn_Hillard.py
+++ Cahn_Hillard.py
@@ -9,15 +9,47 @@ import matplotlib.pyplot as plt
 import time
 from matplotlib import cm
 
+#Goal of the simulation two-dimensional phase-field simulation 
+#of the spinodal decomposition using Cahn-Hilliard equation.
 
 def func_laplacian(c,dx,dy):
     Nx,Ny =c.shape
-    c_top=c[Ny,:],c[1:Ny-1,:]
-    c_bot=c[2:Ny,:],c[1,:]
-    c_lef=c[:,Nx],c[:,1]
-    c_rig=c[:,2:Nx],c[:,1]
-    lap=(c_top+c_bot+c_rig+c_lef-4*c)/(dx*dy)
-    return lap
+    for i,j in zip(Nx,Ny):
+        #nearest and second nearest neigbours 
+        c_center = c[i,j]
+        c_up= c[i,j+1]
+        c_down=c[i,j-1]
+        c_left=c[i-1,j]
+        c_right=c[i+1,j]
+        c_up_right=c[i+1,j+1]
+        c_up_left=c[i-1,j+1]
+        c_up_up=c[i,j+2]
+        c_down_down=c[i,j-2]
+        c_left_left=[i-2,j]
+        c_right_right=c[i+2,j]
+        c_down_left=c[i-1,j-1]
+        c_down_right=c[i+1,j-1]
+        
+        #periodic boundary conditions
+        if c_up > c[i, Ny-1]:
+            c_up = c[i,j+1-Ny]
+        if c_left < c[0,j]:
+            c_left= c[i-1+Nx,j]
+        if c_right > c[Nx-1,j]:
+            c_right = c[i+1-Nx,j]
+        if c_down < c[i,0]:
+            c_down=c[i,j-1+Ny]
+            
+        if c_up_up > c[i, Ny-1]:
+            c_up_up = c[i,j+2-Ny]
+        if c_left_left < c[0,j]:
+            c_left_left= c[i-2+Nx,j]
+        if c_right_right > c[Nx-1,j]:
+            c_right_right = c[i+2-Nx,j]
+        if c_down_down < c[i,0]:
+            c_down_down=c[i,j-2+Ny]
+
+        
     
 def add_fluctuation(Nx, Ny, c0, noise):
     c=c0+noise*(0.5-np.random.rand(Nx,Ny))
@@ -26,23 +58,30 @@ def add_fluctuation(Nx, Ny, c0, noise):
 def chemical_potential(c,A):
    #use the analytic expression for the chemical potential obtained as
    #derivative of the homogeneous free energy
-   mu=[]
-   for i in c:
-       m=2*A*(i*((1-i)^2)-(i^2)*(1-i))
-       mu.append(m)
+   mu=2*A*(c*((1-c)**2)-(c**2)*(1-c))
    return mu
 
 def free_energy(c, A, k, dx, dy):
     Ny, Nx=c.shape
-    c_top=np.concatenate(c[Ny,:],c[1:Ny-1,:])
-    c_left=np.concatenate(c[:,Nx], c[:,1:Nx-1])
+    c=c.tolist()
+    Nx=int(Nx)
+    Ny=int(Ny)
+    c_top=c[Ny-1,:] + c[1:Ny,:]
+    c_left=c[:,Nx] + c[:,1:Nx]
     
-    F=[]
+    C0=[]
+    CL=[]
+    CT=[]
     for c0,ct,cl in zip(c,c_top,c_left):
-        hom_term=dx*dy*A*np.sum( np.sum( (c0^2)*((1-c0)^2)) )
-        grad_term = (k/dx)*np.sum(np.sum ((c0-cl)^2) ) + (k/dy)*np.sum(np.sum ((c0-ct)^2))
-        f=hom_term+grad_term
-        F.append(f)
+        c0_sum= [(c**2)*((1-c)**2) for c in c0]
+        cl_sum=[(c1-c2)**2 for c1,c2 in zip(c0,cl)]
+        ct_sum=[(c1-c2)**2 for c1,c2 in zip(c0,ct)]
+        C0.append(np.sum(c0_sum))
+        CL.append(np.sum(cl_sum))
+        CT.append(np.sum(ct_sum))
+    hom_term=dx*dy*A*np.sum(C0)
+    grad_term = (k/dx)*np.sum(CL) + (k/dy)*np.sum(CT)
+    F=hom_term+grad_term
     return F
         
 # finite difference phase-field code to solve the cahn-hilliard equation
@@ -73,7 +112,7 @@ A = 1.0 #multiplicative constant in free energy
 
 #starting microstructure with a concentration fluctuation
 c=add_fluctuation(Nx,Ny,c0,dc)
-print(c)
+
 
 # initialization of the arrays that track the time evolution;
 n2=round(nstep/nprint)+1
@@ -81,7 +120,11 @@ c_t=np.zeros(n2) # average concentration
 F_t=np.zeros(n2) #free energy
 t_t=np.zeros(n2) #time
 
-c_t[1] = np.mean(np.mean(c))
+c_mean=[]
+for i in c:
+    c_mean.append(np.mean(i))
+c_t[1]=np.mean(c_mean)
+
 F_t[1]=free_energy(c,A,grad_coef,dx,dy)
 
 
@@ -97,3 +140,4 @@ for istep in range(1,nstep):
     
     #time evolution
     c = c + dtime * mobility*lap_dF_dc
+    print(c)
