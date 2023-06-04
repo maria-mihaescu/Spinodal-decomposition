b13e22abc43dadad30e04de7643a7836f9b92ccd
Maria Mihaescu <mariachani.mihaescu@studio.unibo.it>
2023-06-04 18:51:50 +0200

    fixed the printing of the initial plots for the animation and the event reading problem

--- GUI.py
+++ GUI.py
@@ -16,6 +16,14 @@ from Binary_Alloys import interaction_parameter
 from Binary_Alloys import set_free_energy
 from Binary_Alloys import plot_anim_3d, plot_2d
 
+from Cahn_Hillard import diffusion_coeff
+from Cahn_Hillard import time_increment
+from Cahn_Hillard import plot_chemical_potential
+from Cahn_Hillard import initial_concentration
+from Cahn_Hillard import plot_initial_concentration
+from Cahn_Hillard import Cahn_hiliard_animated
+#from Cahn_Hillard import atom_interac_cst
+
 
 def draw_figure(canvas, figure):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
@@ -24,6 +32,7 @@ def draw_figure(canvas, figure):
     return figure_canvas_agg
 
 # Define the window layout
+
 def make_window1():
     layout = [
         [sg.Text("Free energy of a binary alloy in the Quasi-chemical atomistic model")],
@@ -35,7 +44,7 @@ def make_window1():
         [sg.Text('Temperature in [K] for calculation of G in (X_B, eta) space'), sg.InputText()],
        
         [sg.Canvas(key='-FIG0-'),sg.Canvas(key='-FIG1-')],
-        [sg.Button('Ok'),sg.Button('Next >')],
+        [sg.Button('Ok'),sg.Button('Next p2 >')],
     ]
 
     return sg.Window(
@@ -46,74 +55,84 @@ def make_window1():
         element_justification="center")
 
 def make_window2():
+    
     layout = [[sg.Text('3D plots of the free energy in the different spaces')],
-              [sg.Button('Show')],
+              [sg.Button('Show 3D plots')],
               [sg.Canvas(key="-3D_(eta,X_B)-"),sg.Canvas(key="-3D_(X_B,T)-")],
               [sg.Canvas(key="-3D_(eta,T)-")],
-               [sg.Button('< Prev'), sg.Button('Next >')]]
+               [sg.Button('< Prev p1'), sg.Button('Next p3 >')]]
 
     return sg.Window('3D plots of the free energy in the different spaces', layout,location=(0, 0),
     finalize=True,
     element_justification="center")
 
 def make_window3():
+    
     layout = [[sg.Text('2D Spinodal decomposition solving Cahn Hilliards equations')],
               [sg.Text('Parameters specific to the material :')],
               [sg.Text('Average composition of atom B [at. frac]: c0'),sg.InputText()],
               [sg.Text('Temperature of the BiAlloy  [K] : T0'),sg.InputText()],
-              [sg.Text('Atom interaction constant [J/mol] : La'),sg.InputText()],
+              #[sg.Text('Atom interaction constant [J/mol] : La'),sg.InputText()],
               [sg.Text('Gradient coefficient [J*M^2/mol] : A'),sg.InputText()],
-              [sg.Text('Diffusion coefficient of A atoms [M^2/s] : Diff_A'),sg.InputText()],
-              [sg.Text('Diffusion coefficient of B atoms [M^2/s] : Diff_B'),sg.InputText()],
+              
+              [sg.Text('Coefficient for diffusion coefficient calculation of A atoms [M^2/s] : coef_DA'),sg.InputText()],
+              [sg.Text('Activation energy for diffusion coefficient of A atoms [J/mol] : E_DA'),sg.InputText()],
+              [sg.Text('Coefficient for diffusion coefficient calculation of B atoms [M^2/s] : coef_DB'),sg.InputText()],
+              [sg.Text('Activation energy for diffusion coefficient of B atoms [J/mol] : E_DB'),sg.InputText()],
+              
               [sg.Text('Parameters specific to the grid :')],
               [sg.Text('Number of computational grids along the x direction : Nx'),sg.InputText()],
               [sg.Text('Number of computational grids along the y direction : Ny'),sg.InputText()],
               [sg.Text('Spacing of computational grids along the x direction [M] : dx'),sg.InputText()],
               [sg.Text('Spacing of computational grids along the y direction [M] : dy'),sg.InputText()],
+              
               [sg.Text('Parameters specific to the animation :')],
               [sg.Text('Total number of time-steps  : Nsteps'),sg.InputText()],
               [sg.Text('Divisor of Nsteps used for printing : Nprint'),sg.InputText()],
-              [sg.Text('Interval between each frame [ms]: Nprint'),sg.InputText()],
-              [sg.Button('Ok'),sg.Button('< Prev'), sg.Button('Next >')]]
+              [sg.Text('Interval between each frame [ms]: Interval'),sg.InputText()],
+              
+              [sg.Button('values ok'),sg.Button('< Prev p2'), sg.Button('Next p4 >')]]
 
     return sg.Window('Parameters for the spinodal decomposition solving Cahn Hilliards equations', layout, location=(0, 0),
-    finalize=True,
-    element_justification="center")
+    finalize=True, element_justification="center")
 
 def make_window4():
+    
     layout = [[sg.Text('Initial states:')],
-              [sg.Button('Show')],
+              [sg.Button('Show initial plots')],
               [sg.Canvas(key="-c0_chemical_potential-"),sg.Canvas(key="-initial_concentration-")],
-               [sg.Button('< Prev'), sg.Button('Next >')]]
+               [sg.Button('< Prev p3'), sg.Button('Next p5 >')]]
 
     return sg.Window('Initial states of the spinodal decomposition solving Cahn Hilliards equations', layout, location=(0, 0),
-    finalize=True,
-    element_justification="center")
+    finalize=True, element_justification="center")
 
 def make_window5():
+    
     layout = [[sg.Text('Animation of the spinodal decomposition solving Cahn Hilliards equations in 2D:')],
-              [sg.Button('Show')],
+              [sg.Button('Show animation')],
               [sg.Canvas(key="-anim-")],
                [sg.Button('< Prev'), sg.Button('Exit')]]
 
     return sg.Window('Initial states of the spinodal decomposition solving Cahn Hilliards equations', layout, location=(0, 0),
-    finalize=True,
-    element_justification="center")
+    finalize=True, element_justification="center")
 
 
 #Make the first window and set the others to none 
 window1, window2, window3, window4, window5 = make_window1(), None, None, None, None
 
 while True:
+    
+    window,event,values = sg.read_all_windows()
+    #window=window1
     #Read all the imputs of the windows
-    window, event, values = sg.read_all_windows()
-
+    #event1, values1 = window1.read()
+    
     if window==window1:
-        
-        if event == sg.WIN_CLOSED : # if user closes window
+              
+        if event== sg.WIN_CLOSED : # if user closes window
             break
         
-        if event == 'Ok':
+        elif event == 'Ok':
             Z=int(values[0])
             diff_eV=float(values[1])
             T0=float(values[2])
@@ -131,38 +150,37 @@ while True:
             fig0=plot_2d(X_B,G_XB_T,'X_B','G vs X_B for different T, eta=0')
             fig1=plot_2d(eta,G_eta_T,'eta','G vs eta for different T, X_B=0.5')
         
-            
             # Add the plot to the window
             
             draw_figure(window1["-FIG0-"].TKCanvas, fig0)
             draw_figure(window1["-FIG1-"].TKCanvas, fig1)
             
-            
         #if event == '-IN-' :
         #  window['-FIG0-'].update(draw_figure(window["-FIG0-"].TKCanvas, fig0))
          #  window["-FIG1-"].update(draw_figure(window["-FIG1-"].TKCanvas, fig1))
 
-        if event == 'Next >':
+        elif event == 'Next p2 >':
             window1.hide()
             window2 = make_window2()
-            
+            #window=window2
+            #event2, values2 = window2.read()
             
     if window == window2:
-
-        if event == 'Show': 
+        
+        if event == sg.WIN_CLOSED : # if user closes window
+            break
+        
+        elif event == 'Show 3D plots': 
+            
             #Free energy surface in the (X_B,eta) space for temperature T=T0
             fig_3d_0=plot_anim_3d(X_XB_eta,eta_XB_eta,G_XB_eta,
                          'X_B','eta','G [ J/mole ]'
                          ,'G vs X_B and eta')
             
-            draw_figure(window["-3D_(eta,X_B)-"].TKCanvas, fig_3d_0)
-            
             # Free energy surface in (X_B,T) space for order parameter eta=0 
             fig_3d_1=plot_anim_3d(X_XB_T,T_XB_T,G_XB_T,
                          'X_B','T [K]','G [ J/mole ]'
                          ,'G vs X_B and T, eta=0')
-            
-            draw_figure(window["-3D_(X_B,T)-"].TKCanvas, fig_3d_1)
             
             #Free energy surface in (eta,T) space for equimolar composition (X_B=0.5)
         
@@ -170,57 +188,108 @@ while True:
                          'eta','T [K]','G [ J/mole ]'
                          ,'G vs eta and T, X_B=0.5')
             
-            draw_figure(window["-3D_(eta,T)-"].TKCanvas, fig_3d_2)
+            
+            draw_figure(window2["-3D_(eta,X_B)-"].TKCanvas, fig_3d_0)
+            draw_figure(window2["-3D_(X_B,T)-"].TKCanvas, fig_3d_1)
+            draw_figure(window2["-3D_(eta,T)-"].TKCanvas, fig_3d_2)
         
-        if event == sg.WIN_CLOSED : # if user closes window
-            break
-        
-        if event == 'Next >':
+        elif event == 'Next p3 >':
+            print('Next pushed')
             window2.hide()
             window3 = make_window3()
-            
-        elif event in (sg.WIN_CLOSED, '< Prev'):
-            window2.hide()
+            #window=window3
+            #event3, values3 = window3.read()
+
+             
+        elif event =='< Prev p1':
+            window2.close()
             window1.un_hide()
+            #window=window1
             
     if window == window3:
-        #if event == 'Show':
         
         if event == sg.WIN_CLOSED : # if user closes window
             break
         
-        if event == 'Next >':
+        elif event == 'values ok':
+            #setting the variables to the user defined values
+            c0=float(values[0])
+            T=float(values[1])
+            #La=float(values[2])
+            A=float(values[2])
+            coef_DA=float(values[3])
+            E_DA=float(values[4])
+            coef_DB=float(values[5])
+            E_DB=float(values[6])
+            Nx=int(values[7])
+            Ny=int(values[8])
+            dx=float(values[9])
+            dy=float(values[10])
+            nsteps=int(values[11])
+            nprint=int(values[12])
+            interval=float(values[13])
+            
+            #setting the complementary variables that are in function of the set ones
+            La=20000.-9.*T # Atom interaction constant [J/mol]
+            Diff_A = diffusion_coeff(coef_DA,E_DA,T)# diffusion coefficient of A atom [m2/s]
+            Diff_B = diffusion_coeff(coef_DB,E_DB,T) # diffusion coefficient of B atom [m2/s]
+            dt = time_increment(dx,Diff_A)
+            print("Values stored")
+            print(nsteps,nprint,interval,Nx,Ny,A,dx,dy,T,La,Diff_A,Diff_B,dt)
+        elif event == 'Next p4 >':
             window3.hide()
             window4 = make_window4()
+            #window=window4
+            #event4, values4 = window4.read()
             
-        elif event in (sg.WIN_CLOSED, '< Prev'):
-            window3.hide()
+            
+        elif event == '< Prev p2':
+            window3.close()
             window2.un_hide()
-
+            #window=window2
 
     if window == window4:
-        #if event == 'Show': 
-            
-        if event == sg.WIN_CLOSED : # if user closes window 
+        if event== sg.WIN_CLOSED : # if user closes window 
             break
         
-        if event == 'Next >':
+        
+        elif event == 'Show initial plots': 
+            
+            fig_chem_pot=plot_chemical_potential(c0,T,La)
+            draw_figure(window["-c0_chemical_potential-"].TKCanvas, fig_chem_pot)
+            
+            #Defining a random initial concentration
+            c,c_t=initial_concentration(Nx,Ny,c0)
+            fig_init_c=plot_initial_concentration(c)
+            draw_figure(window["-initial_concentration-"].TKCanvas, fig_init_c)
+           
+        
+        elif event == 'Next p5 >':
             window4.hide()
             window5 = make_window5()
+            #window=window5
+            #event5, values5 = window5.read()
             
-        elif event in (sg.WIN_CLOSED, '< Prev'):
-            window4.hide()
+            
+        elif event =='< Prev p4':
+            
+            window4.close()
             window3.un_hide()
+            #window=window3
             
     if window == window5:
-                             
+        
         if event == sg.WIN_CLOSED or event == 'Exit': # if user closes window or clicks cancel
             break
         
-        if event in (sg.WIN_CLOSED, '< Prev'):
-            window4.hide()
-            window3.un_hide()
+        elif event == 'Show animation': 
+            fig_anim=Cahn_hiliard_animated(c,c_t,nsteps,nprint,interval,Nx,Ny,A,dx,dy,T,La,Diff_A,Diff_B,dt) 
+            draw_figure(window["-anim-"].TKCanvas, fig_anim)
+        
+        elif event == '< Prev p4':
+            window5.close()
+            window4.un_hide()
+            #window=window4
         
         
 window.close()
-
