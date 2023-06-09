12ae48598884f11864f30944a09d0a1dba16ecb8
Maria Mihaescu <mariachani.mihaescu@studio.unibo.it>
2023-06-09 13:01:54 +0200

    Starting to add the documentation for each function

--- Cahn_Hillard.py
+++ Cahn_Hillard.py
@@ -7,98 +7,235 @@ Created on Mon May 29 15:46:02 2023
 
 import numpy as np
 import matplotlib
-import matplotlib.animation as animation
-import matplotlib.pyplot as plt
-#Goal of the simulation two-dimensional phase-field simulation 
-#of the spinodal decomposition using Cahn-Hilliard equation.
-#define the simulation cell parameters
 
+
+#Defining the physical constants
 R = 8.314 # gas constant
 
-def atom_interac_cst(T):    
+def atom_interac_cst(T):
+    """
+    Function to calculate the atomic interaction constant between atom A and B in  [J/mol]
+
+    Parameters
+    ----------
+    T : TYPE float
+        DESCRIPTION.Temperature in K
+
+    Returns
+    -------
+    La : TYPE float
+         DESCRIPTION atomic interaction constant between atom A and B in  [J/mol]
+
+    """    
     La = 20000.-9.*T # Atom interaction constant [J/mol]
     return La
 
 
-#parameters specific to the material entered by the user
+def diffusion_coeff(coef,
+                    E,
+                    T):
+    """
+    Exponential function to calculate the diffusion coefficient in [m2/s]
 
-c0 = 0.5 # average composition of B atom [atomic fraction]
-T = 673 # temperature [K]
+    Parameters
+    ----------
+    coef : TYPE float
+        DESCRIPTION. Coefficient used for the calculation of the diffusion coefficient
+    E : TYPE float
+        DESCRIPTION.Energy in J
+    T : TYPE float
+        DESCRIPTION. Temperature in K 
 
-La=13943
-A= 3.0e-14 # gradient coefficient [Jm2/mol]
+    Returns
+    -------
+    Diff : TYPE float 
+        DESCRIPTION. diffusion coefficient in [m2/s]
 
-coef_DA=1.0e-04
-coef_DB=2.0e-05
-E_DA=300000.0
-E_DB=300000.0
-
-
-#Parameters specific to the grid 
-Nx= 32 #number of computational grids along the x direction
-Ny= 32 #number of computational grids along the y direction
-dx =  2.0e-9 # spacing of computational grids [m]
-dy =  2.0e-9 # spacing of computational grids [m]
-
-
-#nsteps,nprint,interval
-
-def diffusion_coeff(coef,E,T):
+    """
     Diff=coef*np.exp(-E/(R*T))
     return Diff
 
+def time_increment(dx,
+                   Diff_A):
+    """
+    Function to calculate the time increment in s between each update of the concentration
 
-Diff_A = diffusion_coeff(coef_DA,E_DA,T)# diffusion coefficient of A atom [m2/s]
-Diff_B = diffusion_coeff(coef_DB,E_DB,T) # diffusion coefficient of B atom [m2/s]
+    Parameters
+    ----------
+    dx : TYPE float
+        DESCRIPTION. spacing of the computational grid in the x direction [m]
+    Diff_A : TYPE float 
+        DESCRIPTION.diffusion coefficient in [m2/s]
 
+    Returns
+    -------
+    dt : TYPE float 
+        DESCRIPTION.time increment [s]
 
-def time_increment(dx,Diff_A):
-    # time increment [s]
+    """
     dt = (dx*dx/Diff_A)*0.1
     return dt
 
-dt = time_increment(dx,Diff_A)
+def add_fluctuation(Nx, 
+                    Ny, 
+                    c0): 
+    """
+    function that adds random fluctuation of the concentration in a computational grid of size
+    Nx*Ny, starting from an initial concentration of B atoms of c0 in [atomic fraction]
 
-def add_fluctuation(Nx, Ny, c0):  
+    Parameters
+    ----------
+    Nx : TYPE int
+        DESCRIPTION. number of computational grids along the x direction
+    Ny : TYPE int
+        DESCRIPTION. number of computational grids along the y direction
+    c0 : TYPE float
+        DESCRIPTION. average composition of B atom [atomic fraction]
+
+    Returns
+    -------
+    c : TYPE list 
+        DESCRIPTION. Composition matrix where c[x,y] is the composition at point of coordinate (x,y)
+
+    """
     c = c0 + np.random.rand(Nx, Ny)*0.01
     return c
 
-def func_laplacian(center,left,right,up,down,dx,dy):
+def func_laplacian(center,
+                   left,
+                   right,
+                   up,
+                   down,
+                   dx,
+                   dy):
+    """
+    Calculates the gradient term of a quantity at a given matrix point
+    (concentration or diffusion potential) given the variation of the quantity 
+    within its nearest neighbours in the matrix of size Nx*Ny.
+
+    Parameters
+    ----------
+    center : TYPE float
+        DESCRIPTION. quantity at the point (x,y)
+    left : TYPE float
+        DESCRIPTION. quantity at the point (x-1,y)
+    right : TYPE float
+        DESCRIPTION. quantity at the point (x+1,y)
+    up : TYPE float
+        DESCRIPTION. quantity at the point (x,y+1)
+    down : TYPE float
+        DESCRIPTION. quantity at the point (x,y-1)
+    dx : TYPE float
+        DESCRIPTION. spacing of the computational grid in the x direction [m]
+    dy : TYPE float
+        DESCRIPTION. spacing of the computational grid in the y direction [m]
+
+    Returns
+    -------
+    lap : TYPE float
+        DESCRIPTION. gradient term of the quantity at a matrix point
+
+    """
     lap=(right-2*center+left)/dx/dx + (up -2*center+down) /dy/dy
     return lap
 
-def chemical_potential(c,T,La):
+def chemical_free_energy_density(c,
+                       T,
+                       La):
+    """
+    Function to calculate the chemical potential  in a specific point given a concentration of B atom, a temperature and 
+    and atomic interaction constante
+
+    Parameters
+    ----------
+    c : TYPE float
+        DESCRIPTION. composition of B atom at the specific point [atomic fraction]
+    T : TYPE float
+        DESCRIPTION. Temperature of the material [K]
+    La : TYPE float 
+        DESCRIPTION.atomic interaction constante [J/mol]
+
+    Returns
+    -------
+    chem_pot : TYPE float
+        DESCRIPTION. Chemical free energy density
+
+    """
     chem_pot= R*T*(c*np.log(c)+(1-c)*np.log(1-c))+La*c*(1-c)
     return chem_pot
 
 
-def plot_chemical_potential(c0,La):
+def plot_chemical_free_energy_density(c0,
+                            La):
+    """
+    Function to plot the chemical free energy density in function of concentration 
+    for c between [0,1] and T=673K to show where the initial concentration of B atoms c0 stands 
+
+    Parameters
+    ----------
+    c0 : TYPE float
+        DESCRIPTION. average composition of B atom [atomic fraction]
+    La : TYPE float 
+        DESCRIPTION.Atom intaraction constant [J/mol]
+
+    Returns
+    -------
+    fig : TYPE matplotlib.figure.Figure()
+        DESCRIPTION. figure of the Chemical free energy density in function of concentration
+        for c between [0,1] and T=673K
+
+    """
     fig = matplotlib.figure.Figure()
-    #fig=plt.figure()
     ax = fig.add_subplot()
+    
     T=673 
     cc = np.linspace(0.01, 0.99, 100)
-    ax.plot(cc,chemical_potential(cc,T,La),color='black')
-    ax.plot(c0, chemical_potential(c0,T,La),color='r',marker='o',markersize=10)
+    
+    ax.plot(cc,chemical_free_energy_density(cc,T,La),color='black')
+    ax.plot(c0, chemical_free_energy_density(c0,T,La),color='r',marker='o',markersize=10)
+    
     ax.set_xlabel('Concentration c [at. frac]')
     ax.set_ylabel('Chemical free energy density')
-    #fig.show()
+    ax.set_title('Chemical free energy density in function of concentration \n for c between [0,1] and T=673K')
+    
     return fig
 
 
-#plot_chemical_potential(c0,T,La)
-#plt.show()
+def boundary_conditions(x,
+                        y,
+                        Nx,
+                        Ny):
+    """
+    Function to define the boundary conditions of the Nx*Ny matrix. If a border is reached, one goes 
+    to the border on the other side. 
 
-def atom_interac_cst(T):    
-    La = 20000.-9.*T # Atom interaction constant [J/mol]
-    return La
+    Parameters
+    ----------
+    x : TYPE
+        DESCRIPTION.
+    y : TYPE
+        DESCRIPTION.
+    Nx : TYPE
+        DESCRIPTION.
+    Ny : TYPE
+        DESCRIPTION.
 
-def time_increment(dx,Diff_A):
-    # time increment [s]
-    dt = (dx*dx/Diff_A)*0.1
-    return dt
+    Returns
+    -------
+    x : TYPE
+        DESCRIPTION.
+    y : TYPE
+        DESCRIPTION.
+    x_plus : TYPE
+        DESCRIPTION.
+    x_min : TYPE
+        DESCRIPTION.
+    y_plus : TYPE
+        DESCRIPTION.
+    y_min : TYPE
+        DESCRIPTION.
 
-def boundary_conditions(x,y,Nx,Ny):
+    """
     #Renaming the coordinates
     x=x
     y=y
@@ -120,7 +257,41 @@ def boundary_conditions(x,y,Nx,Ny):
         
     return x,y,x_plus,x_min,y_plus,y_min
 
-def concentration_nearest_neighbours(c,x,y,Nx,Ny):
+def concentration_nearest_neighbours(c,
+                                     x,
+                                     y,
+                                     Nx,
+                                     Ny):
+    """
+    
+
+    Parameters
+    ----------
+    c : TYPE
+        DESCRIPTION.
+    x : TYPE
+        DESCRIPTION.
+    y : TYPE
+        DESCRIPTION.
+    Nx : TYPE
+        DESCRIPTION.
+    Ny : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    c_center : TYPE
+        DESCRIPTION.
+    c_left : TYPE
+        DESCRIPTION.
+    c_right : TYPE
+        DESCRIPTION.
+    c_up : TYPE
+        DESCRIPTION.
+    c_down : TYPE
+        DESCRIPTION.
+
+    """
     
     x,y,x_plus,x_min,y_plus,y_min = boundary_conditions(x, y,Nx,Ny)
         
@@ -133,13 +304,74 @@ def concentration_nearest_neighbours(c,x,y,Nx,Ny):
     
     return c_center,c_left,c_right,c_up,c_down
 
-def diffusion_potential_chemical(cc,T,La):
+def diffusion_potential_chemical(cc,
+                                 T,
+                                 La):
+    """
+    
+
+    Parameters
+    ----------
+    cc : TYPE
+        DESCRIPTION.
+    T : TYPE
+        DESCRIPTION.
+    La : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    mu_chem_dir : TYPE
+        DESCRIPTION.
+
+    """
     #chemical term of the diffusion potential
     mu_chem_dir= R*T*(np.log(cc)-np.log(1.0-cc))+ La*(1.0-2.0*cc)
     return mu_chem_dir
 
 
-def total_diffusion_potential(c,x,y,A,dx,dy,T,La,Nx,Ny):
+def total_diffusion_potential(c,
+                              x,
+                              y,
+                              A,
+                              dx,
+                              dy,
+                              T,
+                              La,
+                              Nx,
+                              Ny):
+    """
+    
+
+    Parameters
+    ----------
+    c : TYPE
+        DESCRIPTION.
+    x : TYPE
+        DESCRIPTION.
+    y : TYPE
+        DESCRIPTION.
+    A : TYPE
+        DESCRIPTION.
+    dx : TYPE
+        DESCRIPTION.
+    dy : TYPE
+        DESCRIPTION.
+    T : TYPE
+        DESCRIPTION.
+    La : TYPE
+        DESCRIPTION.
+    Nx : TYPE
+        DESCRIPTION.
+    Ny : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    mu_tot_dir : TYPE
+        DESCRIPTION.
+
+    """
     
     #getting the concentration of the neighrest neighbours around the point in (x,y)
     c_center,c_left,c_right,c_up,c_down = concentration_nearest_neighbours(c,x,y,Nx,Ny)
@@ -156,7 +388,54 @@ def total_diffusion_potential(c,x,y,A,dx,dy,T,La,Nx,Ny):
     
     return mu_tot_dir
     
-def update_order_parameter(c,c_t,Nx,Ny,A,dx,dy,T,La,Diff_A,Diff_B,dt):
+def update_order_parameter(c,
+                           c_t,
+                           Nx,
+                           Ny,
+                           A,
+                           dx,
+                           dy,
+                           T,
+                           La,
+                           Diff_A,
+                           Diff_B,
+                           dt):
+    """
+    
+#Goal of the simulation two-dimensional phase-field simulation 
+#of the spinodal decomposition using Cahn-Hilliard equation.
+    Parameters
+    ----------
+    c : TYPE
+        DESCRIPTION.
+    c_t : TYPE
+        DESCRIPTION.
+    Nx : TYPE
+        DESCRIPTION.
+    Ny : TYPE
+        DESCRIPTION.
+    A : TYPE
+        DESCRIPTION.
+    dx : TYPE
+        DESCRIPTION.
+    dy : TYPE
+        DESCRIPTION.
+    T : TYPE
+        DESCRIPTION.
+    La : TYPE
+        DESCRIPTION.
+    Diff_A : TYPE
+        DESCRIPTION.
+    Diff_B : TYPE
+        DESCRIPTION.
+    dt : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
     
     for j in range(Ny):
         for i in range(Nx):
@@ -204,7 +483,29 @@ def update_order_parameter(c,c_t,Nx,Ny,A,dx,dy,T,La,Diff_A,Diff_B,dt):
 
 
 #Solving the Cahn-hiliard equation and plotting the result
-def initial_concentration(Nx,Ny,c0):
+def initial_concentration(Nx,
+                          Ny,
+                          c0):
+    """
+    
+
+    Parameters
+    ----------
+    Nx : TYPE
+        DESCRIPTION.
+    Ny : TYPE
+        DESCRIPTION.
+    c0 : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    c : TYPE
+        DESCRIPTION.
+    c_t : TYPE
+        DESCRIPTION.
+
+    """
     #Setting concentrations to 0
     c= np.zeros((Nx,Ny))
     c_t= np.zeros((Nx,Ny))
@@ -215,6 +516,20 @@ def initial_concentration(Nx,Ny,c0):
     return c,c_t
 
 def plot_initial_concentration(c):
+    """
+    
+
+    Parameters
+    ----------
+    c : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    fig : TYPE
+        DESCRIPTION.
+
+    """
     #printing separatly the initial concentration plot
     fig = matplotlib.figure.Figure()
     ax = fig.add_subplot()
@@ -224,6 +539,20 @@ def plot_initial_concentration(c):
     return fig
 
 def plot_concentration(c):
+    """
+    
+
+    Parameters
+    ----------
+    c : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    fig : TYPE
+        DESCRIPTION.
+
+    """
     #printing separatly the initial concentration plot
     fig = matplotlib.figure.Figure()
     ax = fig.add_subplot()
@@ -232,38 +561,4 @@ def plot_concentration(c):
     fig.colorbar(im,ax=ax)
     return fig
 
-def Cahn_hiliard_animated(c,c_t,nsteps,nprint,interval,Nx,Ny,A,dx,dy,T,La,Diff_A,Diff_B,dt):
-    
-    # Plot animated 
-    # Time integration parameters
-    
-    #creating the snapshots list with all the images
-    
-    fig = matplotlib.figure.Figure()
-    ax = fig.add_subplot()
-    cbar=None
-    snapshots=[]
-    
-    for istep in range(1,nsteps+1):
-        update_order_parameter(c,c_t,Nx,Ny,A,dx,dy,T,La,Diff_A,Diff_B,dt)
-        c[:,:]=c_t[:,:] # updating the order parameter every dt 
-    
-        if istep % nprint ==0:
-            if cbar:
-                cbar.remove()
-            im = ax.imshow(c, cmap='bwr', animated=True)
-            cbar=fig.colorbar(im,ax=ax)    
-            snapshots.append([im])       
-
-    anim = animation.ArtistAnimation(fig,snapshots,interval, blit=True,repeat_delay=10)
-    return anim
-    
-
-"""
-#run the programm 
-c,c_t=initial_concentration(Nx,Ny)
-plot_initial_concentration(c)
-Cahn_hiliard_animated(c,c_t,600,60,700,Nx,Ny,A,dx,dy,T,La,Diff_A,Diff_B,dt)                        
-        
-"""
-            
+   
