8d8d5c8b287e18ba48b541b74fb53270b543274e
Maria Mihaescu <mariachani.mihaescu@studio.unibo.it>
2023-06-10 15:50:40 +0200

    broke down functions of free energy in smaller functions to write the test functions

--- Binary_Alloys.py
+++ Binary_Alloys.py
@@ -50,6 +50,14 @@ def xlog(x):
 
 
 def test_xlog():
+    """
+    function to test the x*log() calculation
+
+    Returns
+    -------
+    None.
+
+    """
     # Test case 1
     X1 = np.array([0.2, 0.5, 0.8])
     expected_result1 = np.array([-0.32188758, -0.34657359, -0.17851484])
@@ -70,36 +78,6 @@ def test_xlog():
 
     print("All tests passed successfully!")
     
-def xlog_scal(x):
-
-    """
-    function for the fomula x*log(x) if x is a float
-
-    Parameters
-    ----------
-    x : TYPE : float
-        DESCRIPTION.parameter to be entered in the calculation function
-
-    Returns
-    -------
-    s : TYPE : float
-        DESCRIPTION. calculated parameter
-
-    """
-    if x<0:
-        s=np.nan
-    else:
-        s= x * np.log(x)
-    return s
-    
-def test_xlog_scal():
-    x = 0.5
-    expected_result = -0.3466
-
-    result = xlog_scal(x)
-
-    assert np.isclose(round(result, 4), expected_result), "xlog_scal test failed"
- 
     
 def plot_anim_3d(X,
                  Y,
@@ -156,6 +134,14 @@ def plot_anim_3d(X,
     return fig
     
 def test_plot_anim_3d():
+    """
+    function to test the ploting of the 3D plot with arrays
+
+    Returns
+    -------
+    None.
+
+    """
     X = np.array([[0, 1], [0, 1]])
     Y = np.array([[0, 0], [1, 1]])
     Z = np.array([[0, 1], [1, 0]])
@@ -205,6 +191,14 @@ def plot_2d(X,
     return fig
     
 def test_plot_2d():
+    """
+    Test the 2D plot for an energy G
+
+    Returns
+    -------
+    None.
+
+    """
     X = np.array([0, 1, 2, 3, 4])
     G = np.array([[0, 1, 2, 3, 4], [1, 2, 3, 4, 5], [2, 3, 4, 5, 6]])
     xlabel = "X"
@@ -240,6 +234,14 @@ def interaction_parameter(Z,
     return omega
 
 def test_interaction_parameter():
+    """
+    test the calculation of the interaction parameter 
+
+    Returns
+    -------
+    None.
+
+    """
     Z = 10
     diff_eV = 0.5
     expected_result = -8e-18
@@ -247,6 +249,124 @@ def test_interaction_parameter():
     result = interaction_parameter(Z, diff_eV)
 
     assert np.isclose(result, expected_result), "interaction_parameter test failed"
+
+
+def enthalpy_scal(eta,composition,omega,T0):
+    """
+    function that calculates the enthalpy and finds the unphysical region in the 
+    (X_B, eta) mesh. As abs(eta) must be lower than X_B and 1-X_B (as stated in the model).
+    The enthalpy is set to "Not a Number" in the unphysical region, to avoid plotting of non physical values
+
+    Parameters
+    ----------
+    eta : TYPE float
+          DESCRIPTION. Order parameter (structural)
+    composition : TYPE float
+                  DESCRIPTION. composition [atomic fraction]
+    omega : TYPE float
+            DESCRIPTION.In teraction parameter in J 
+    T0 : TYPE float
+        DESCRIPTION. temperature in K
+
+    Returns
+    -------
+    h : TYPE float
+        DESCRIPTION. enthalpy
+
+    """
+    x=composition
+    e=eta
+    if abs(e)>=x or abs(e)>=(1-x):
+        h=np.nan
+    else:
+        h=N_A*omega*(x*(1-x)+e**2)
+    return h
+
+
+def test_enthalpy_scal():
+    
+    """
+    test the calculation of the enthalpy
+
+    Returns
+    -------
+    None.
+
+    """
+    eta = 0.2  # example value for eta
+    composition = 0.5  # example value for composition
+    omega = 1.5  # example value for omega
+    T0 = 300  # example value for T0
+
+    h = enthalpy_scal(eta, composition, omega, T0)
+
+    # Calculate the expected enthalpy value
+    if abs(eta) >= composition or abs(eta) >= (1 - composition):
+        expected_h = np.nan
+    else:
+        expected_h = N_A * omega * (composition * (1 - composition) + eta ** 2)
+
+    # Assert that the calculated enthalpy matches the expected value
+    assert np.isnan(h) and np.isnan(expected_h) or np.isclose(h, expected_h)
+
+
+def entropie_scal(eta,composition,omega,T0):
+    
+    """
+    function that calculates the entropie and finds the unphysical region in the 
+    (X_B, eta) mesh. As abs(eta) must be lower than X_B and 1-X_B (as stated in the model).
+    The entropy is set to "Not a Number" in the unphysical region, to avoid plotting of non physical values
+
+    Parameters
+    ----------
+    eta : TYPE float
+          DESCRIPTION. Order parameter (structural)
+    composition : TYPE float
+                  DESCRIPTION. composition [atomic fraction]
+    omega : TYPE float
+            DESCRIPTION.In teraction parameter in J 
+    T0 : TYPE float
+        DESCRIPTION. temperature in K
+
+    Returns
+    -------
+    s : TYPE float
+        DESCRIPTION. entropie
+
+    """
+    x=composition
+    e=eta
+    
+    if abs(e)>=x or abs(e)>=(1-x):
+        s=np.nan
+    else:
+        s=-(R_gas/2)*(xlog(x+e)+xlog(x-e)+ xlog(1-x+e)+xlog(1-x-e))
+    return s
+
+def test_entropie_scal():
+    """
+    function to test the validity condition and the calculation of the entropie
+    Returns
+    -------
+    None.
+
+    """
+    eta = 0.2  # example value for eta
+    composition = 0.5  # example value for composition
+    omega = 1.5  # example value for omega
+    T0 = 300  # example value for T0
+
+    s = entropie_scal(eta, composition, omega, T0)
+
+    # Calculate the expected entropy value
+    if abs(eta) >= composition or abs(eta) >= (1 - composition):
+        expected_s = np.nan
+    else:
+        expected_s = -(R_gas / 2) * (xlog(composition + eta) + xlog(composition - eta) +
+                                     xlog(1 - composition + eta) + xlog(1 - composition - eta))
+
+    # Assert that the calculated entropy matches the expected value
+    assert np.isnan(s) and np.isnan(expected_s) or np.isclose(s, expected_s)
 
 
 def free_energy_XB_eta(T0,omega):
@@ -287,22 +407,11 @@ def free_energy_XB_eta(T0,omega):
         h0=[]
         s0=[]
         g0=[]
-         
-        #  Finds the unphysical region in the (X_B, eta) mesh. In fact, abs(eta)
-        #  must be lower than X_B and 1-X_B (as stated in the model)
-        # Sets enthalpy and entropy to "Not a Number" in the unphysical region, to
-        # avoid plotting of non physical values
       
         for x,e in zip(xbg,etag):
-            if abs(e)>=x or abs(e)>=(1-x):
-                h=np.nan
-                s=np.nan
-                g=np.nan
-                 
-            else:
-                h=N_A*omega*(x*(1-x)+e**2)
-                s=-(R_gas/2)*(xlog_scal(x+e)+xlog_scal(x-e)+ xlog_scal(1-x+e)+xlog_scal(1-x-e))
-                g=h-T0*s
+            h=enthalpy_scal(e,x,omega,T0)
+            s=entropie_scal(e,x,omega,T0)
+            g=h-T0*s
                  
             h0.append(h)
             s0.append(s)
@@ -313,12 +422,68 @@ def free_energy_XB_eta(T0,omega):
         G0.append(g0) #Gibbs free energy
      
     #rename variable in function of the space 
+    
     X_XB_eta= x_BG
     eta_XB_eta= etaG
     G_XB_eta= np.array(G0)
-    return X_XB_eta, eta_XB_eta, G_XB_eta
     
+    return X_XB_eta, eta_XB_eta, G_XB_eta
+
+def enthalpie_XB_T(X_XB_T,T0,omega):
+    
+    """
+    function to caculate the enthalpie in the (XB,T) space 
+
+    Parameters
+    ----------
+    X_XB_T : TYPE array
+        DESCRIPTION. Composition in the (XB,T) space
+    T0 : TYPE float 
+        DESCRIPTION. Temperature for calculation of free energy surface in (X_B,eta) space
+    omega : TYPE float
+        DESCRIPTION. Interaction parameter in J 
+
+
+    Returns
+    -------
+    h : TYPE array
+        DESCRIPTION. enthalpie in the (XB,T) space 
+
+    """
+    
+    h= N_A*omega*(np.multiply(X_XB_T,(1-X_XB_T)))
+    
+    return h
+
+def T_entropie_XB_T(X_XB_T, T_XB_T,T0,omega):
+    
+    """
+    function to calculate the temperature times entropie in the (XB,T) space
+
+    Parameters
+    ----------
+    X_XB_T : TYPE array
+        DESCRIPTION. composition in the (X_B,T) space
+    T_XB_T : TYPE array
+        DESCRIPTION. temperature in [K] in the (X_B,T) space
+    T0 : TYPE float 
+        DESCRIPTION. Temperature for calculation of free energy surface in (X_B,eta) space
+    omega : TYPE float
+        DESCRIPTION. Interaction parameter in J 
+
+    Returns
+    -------
+    ts : TYPE array
+        DESCRIPTION. temperature*Entropie
+
+    """
+    
+    ts=-R_gas*(np.multiply(T_XB_T,(xlog(X_XB_T)+xlog(1-X_XB_T))))
+    
+    return ts
+
 def free_energy_XB_T(T0,omega):
+    
     """
     This function calculates the free energy of a binary alloy in the "quasi-chemical" atomistic model.
     The assumptions taken in the model are :
@@ -347,18 +512,69 @@ def free_energy_XB_T(T0,omega):
     """
     # Functions in (X_B,T) space for eta=0
     
-    xG, yG = np.meshgrid(X_B,T)
-    h1=N_A*omega*(np.multiply(xG,(1-xG)))
-    ts1=-R_gas*(np.multiply(yG,(xlog(xG)+xlog(1-xG))))
-    g1=h1-ts1
+    X_XB_T, T_XB_T = np.meshgrid(X_B,T)
+    h1=enthalpie_XB_T(X_XB_T,T0,omega)
+    ts1=T_entropie_XB_T(X_XB_T, T_XB_T,T0,omega)
+    G_XB_T=h1-ts1
     
-    #rename variable in function of the space 
-    X_XB_T= xG
-    T_XB_T= yG
-    G_XB_T= g1
     return X_XB_T,T_XB_T,G_XB_T
 
+
+def enthalpie_eta_T(eta_eta_T,T0,omega):
+    
+    """
+    function to caculate the enthalpie in the (XB,T) space 
+
+    Parameters
+    ----------
+    eta_eta_T : TYPE array
+        DESCRIPTION. structural order parameter in the (eta,T) space
+    T0 : TYPE float 
+        DESCRIPTION. Temperature for calculation of free energy surface in (X_B,eta) space
+    omega : TYPE float
+        DESCRIPTION. Interaction parameter in J 
+
+
+    Returns
+    -------
+    h : TYPE array
+        DESCRIPTION. enthalpie in the (XB,T) space 
+
+    """
+    
+    h= N_A*omega*(0.25+np.multiply(eta_eta_T,eta_eta_T))
+    
+    return h
+
+def T_entropie_eta_T(eta_eta_T,T_XB_T,T0,omega):
+    
+    """
+    function to calculate the temperature times entropie in the (eta,T) space
+
+    Parameters
+    ----------
+    eta_eta_T : TYPE array
+        DESCRIPTION. structural order parameter in the (eta,T) space
+    T_XB_T : TYPE numpy.ndarray
+        DESCRIPTION. Temperature T in the (X_B,T) space 
+    T0 : TYPE float 
+        DESCRIPTION. Temperature for calculation of free energy surface in (X_B,eta) space
+    omega : TYPE float
+        DESCRIPTION. Interaction parameter in J 
+
+    Returns
+    -------
+    ts : TYPE array
+        DESCRIPTION. temperature*Entropie
+
+    """
+    
+    ts=-R_gas*(np.multiply(T_XB_T,(xlog(0.5+eta_eta_T)+xlog(0.5-eta_eta_T))))
+    
+    return ts
+
 def free_energy_eta_T(T0,omega):
+    
     """
     This function calculates the free energy of a binary alloy in the "quasi-chemical" atomistic model.
     The assumptions taken in the model are :
@@ -386,17 +602,15 @@ def free_energy_eta_T(T0,omega):
     """
     
     X_XB_T,T_XB_T,G_XB_T = free_energy_XB_T(T0,omega)
+    
     #Functions in (eta,T) space for X_B=0.5
     
-    eG, TG = np.meshgrid(eta,T)
+    eta_eta_T, T_eta_T = np.meshgrid(eta,T)
     
-    h2=N_A*omega*(0.25+np.multiply(eG,eG))
-    ts2=-R_gas*(np.multiply(T_XB_T,(xlog(0.5+eG)+xlog(0.5-eG))))
-    g2=h2-ts2
-
-    #rename variable in function of the space 
-    eta_eta_T= eG
-    T_eta_T= TG
-    G_eta_T= g2
+    h2=enthalpie_eta_T(eta_eta_T, T0, omega)
+    ts2=T_entropie_eta_T(eta_eta_T, T_XB_T, T0, omega)
+    G_eta_T=h2-ts2
     
     return eta_eta_T, T_eta_T, G_eta_T
+
+
