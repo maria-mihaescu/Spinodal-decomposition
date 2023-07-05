291bdc091bcd19a543b5af7dcf7a3fa9b0c29447
Maria Mihaescu <mariachani.mihaescu@studio.unibo.it>
2023-07-05 19:00:04 +0200

    Added a function to setup the initial plots for the spinodal decomposition

--- GUI_Setup.py
+++ GUI_Setup.py
@@ -10,9 +10,21 @@ from Binary_Alloys import free_energy_XB_T
 from Binary_Alloys import free_energy_eta_T
 from Binary_Alloys import plot_anim_3d, plot_2d
 
+from Cahn_Hillard import diffusion_coeff
+from Cahn_Hillard import time_increment
+from Cahn_Hillard import plot_chemical_free_energy_density
+from Cahn_Hillard import initial_composition
+from Cahn_Hillard import plot_initial_composition
+from Cahn_Hillard import update_order_parameter
+from Cahn_Hillard import atom_interac_cst
+
 # Set-up of the first part on the Binary Allow
 
-def setup_binary_alloy_fig2D(Z,diff_eV,T0,X_B,eta):
+def setup_binary_alloy_fig2D(Z,
+                             diff_eV,
+                             T0,
+                             X_B,
+                             eta):
     """
     Function to setup the figures for the 2D representation of the binary alloy parameters in the GUI,
     with the user entered parameters. 
@@ -50,7 +62,11 @@ def setup_binary_alloy_fig2D(Z,diff_eV,T0,X_B,eta):
     fig1=plot_2d(eta,G_eta_T,'eta','G vs eta for different T, X_B=0.5')
     return fig0, fig1
 
-def setup_binary_alloy_fig3D(Z,diff_eV,T0,X_B,eta):
+def setup_binary_alloy_fig3D(Z,
+                             diff_eV,
+                             T0,
+                             X_B,
+                             eta):
     """
     Function to setup the figures for the 3D representation of the binary alloy parameters in the GUI,
     with the user entered parameters. 
@@ -105,3 +121,39 @@ def setup_binary_alloy_fig3D(Z,diff_eV,T0,X_B,eta):
                  'eta','T [K]','G [ J/mole ]'
                  ,'G vs eta and T, X_B=0.5')
     return fig_3d_0,fig_3d_1,fig_3d_2
+
+def setup_initial_composition_plots(Nx,
+                              Ny,
+                              c0,
+                              La):
+    """
+    Function that return the plots and image describing the initial composition from which
+    the spinodal decomposition starts from
+
+    Parameters
+    ----------
+    Nx : TYPE int
+        DESCRIPTION. number of computational grids along the x direction
+    Ny : TYPE int
+        DESCRIPTION. number of computational grids along the y direction
+    c0 : TYPE float
+        DESCRIPTION. average composition of B atom [atomic fraction]
+    La : TYPE float 
+        DESCRIPTION.atomic interaction constante [J/mol]
+
+    Returns
+    -------
+    fig_chem_pot : figure
+        DESCRIPTION. figure of the Chemical free energy density in function of composition
+        for c between [0,1] and T=673K
+    fig_init_c : TYPE figure
+        DESCRIPTION. Figure of the initial composition
+
+    """
+    #Defining a random initial composition
+    c,c_t=initial_composition(Nx,Ny,c0)
+    #set the figure with the initial data
+    fig_chem_pot=plot_chemical_free_energy_density(c0,La)
+    fig_init_c=plot_initial_composition(c)
+    
+    return fig_chem_pot,fig_init_c
